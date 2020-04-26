709c6e44c91fee27ecf4aeb55f7d85d53295141b
Mr Badmash <63794512+Mrbadmash@users.noreply.github.com>
2020-04-27 04:00:57 +0500

    Update afridi.py

--- afridi.py
+++ afridi.py
@@ -515,7 +515,7 @@ def pilih_super():
 	p = ThreadPool(30)
 	p.map(main, id)
 	print "\033[1;95m•◈•▬ ▬ ▬ ▬ ▬ ▬ ▬•◈•\033[1;91mIjazAfridi\033[1;95m•◈•▬ ▬ ▬ ▬ ▬ ▬ ▬•◈•"
-	print "  \033[1;91m«---•◈•---Developed By Rana Aahil--•◈•---»" #Dev:Mrbadmash
+	print "  \033[1;91m«---•◈•---Developed Ijaz Afridi--•◈•---»" #Dev:Mrbadmash
 	print '\033[1;93m✅Process Has Been Completed Press➡ Ctrl+Z.↩ Next Type (python2 afridi.py)↩\033[1;97m....'
 	print"\033[1;91mTotal OK/\x1b[1;95mCP \033[1;93m: \033[1;91m"+str(len(oks))+"\033[1;93m/\033[1;96m"+str(len(cekpoint))
 	print """
